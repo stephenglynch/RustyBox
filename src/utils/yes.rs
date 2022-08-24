1737e6b520d9f609aafc9447ca468d304ab77d09
Stephen Lynch <stepheng.lynch@gmail.com>
2022-08-24 20:34:02 +0100

    Added completed yes

--- src/utils/yes.rs
+++ src/utils/yes.rs
@@ -1,8 +1,21 @@
-use std::ffi::OsString;
+use std::ffi::{OsString, OsStr};
+use std::io::{stdout, Write, self};
+use std::os::unix::prelude::OsStrExt;
 
 
-pub fn yes_main(_args: Vec<OsString>) -> i32 {
+fn write_line(s: &[u8]) -> io::Result<()> {
+    stdout().write_all(s)?;
+    stdout().write_all("\n".as_bytes())
+}
+
+pub fn yes_main(args: Vec<OsString>) -> i32 {
+    let mut yes_val = OsString::from("y");
+    if args.len() > 0 {
+        let sep = OsStr::new(" ");
+        yes_val = args.join(sep);
+    }
+
     loop {
-        println!("y");
+        write_line(yes_val.as_bytes()).expect("Yes ran into an issue.");
     }
 }
