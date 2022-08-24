0237dc982f9985662a1354ab54ddaf23d7cc88e9
Stephen Lynch <stepheng.lynch@gmail.com>
2022-08-24 20:34:28 +0100

    Added basename utility

---
+++ src/utils/basename.rs
@@ -0,0 +1,47 @@
+use std::ffi::{OsString, OsStr};
+use std::io::{stdout, Write, self};
+use std::os::unix::prelude::OsStrExt;
+use std::path::Path;
+
+
+fn strip_suffix<'a, 'b>(s: &'a OsStr, suffix: &'b OsStr) -> &'a OsStr {
+    let s = s.as_bytes();
+    let suffix = suffix.as_bytes();
+    match s.strip_suffix(suffix) {
+        None => OsStr::from_bytes(s),
+        Some(x) => OsStr::from_bytes(x)
+    }
+}
+
+fn write_line(s: &[u8]) -> io::Result<()> {
+    stdout().write_all(s)?;
+    stdout().write_all("\n".as_bytes())
+}
+
+pub fn basename_main(args: Vec<OsString>) -> i32 {
+    if args.len() == 0 {
+        println!("basename: missing operand");
+        return 1;
+    }
+
+    if args.len() > 2 {
+        println!("basename: too many operands");
+        return 1;
+    }
+
+    let empty = OsString::from("");
+    let path = Path::new(&args[0]);
+    let suffix = args.get(1).unwrap_or(&empty);
+
+    // Get filename
+    let file_name = match path.file_name() {
+        Some(fname) => fname,
+        None => OsStr::new("..")
+    };
+
+    // Remove suffix
+    let basename = strip_suffix(file_name, suffix);
+
+    write_line(basename.as_bytes());
+    return 0;
+}
