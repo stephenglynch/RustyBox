824e7d27495154668e9317e855ddf68e1c4705d4
Stephen Lynch <stepheng.lynch@gmail.com>
2022-08-24 20:33:50 +0100

    Added true and false utils

---
+++ src/utils/false.rs
@@ -0,0 +1,6 @@
+use std::ffi::OsString;
+
+
+pub fn false_main(_args: Vec<OsString>) -> i32 {
+    return 1;
+}
---
+++ src/utils/true.rs
@@ -0,0 +1,6 @@
+use std::ffi::OsString;
+
+
+pub fn true_main(_args: Vec<OsString>) -> i32 {
+    return 0;
+}
